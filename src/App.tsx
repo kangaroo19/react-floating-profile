639699ac0a47f889184105c4cb24b053f7a54976
kangaroo19 <1000jjj@naver.com>
2025-02-11 21:23:01 +0900

    [ERROR]  ***에러 인지***
    tsc 명령어 수행 시 css 파일은 대상이 안됨
    css 파일 직접 복붙하여 dist 폴더에 넣으니 잘됨
    일단 개발 진행하기로 결정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import ReactFloatingProfile from "../dist/lib/react-floating-profile/ui/ReactFloatingProfile";
 
 function App() {
   return (
     <>
+      <ReactFloatingProfile username="kangaroo19" />
     </>
   );
 }
 
 export default App;
+// https://vccolombo.github.io/blog/tsc-how-to-copy-non-typescript-files-when-building/
