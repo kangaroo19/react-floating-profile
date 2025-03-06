6ac31a1f7fd747f363dc23b16e16573a3745dd5d
천짱 <86513078+kangaroo19@users.noreply.github.com>
2025-03-06 17:55:54 +0900

    [ERROR] 에러 발견

--- src/lib/react-floating-profile/ui/components/PinnedRepo.tsx
+++ src/lib/react-floating-profile/ui/components/PinnedRepo.tsx
@@ -5,17 +5,21 @@ interface PinnedRepoItemPropTypes {
 }
 export default function PinnedRepo({ pinnedRepoArr }: PinnedRepoItemPropTypes) {
   return (
-    <div className="repo-container">
-      <div className="repo-label">My Works</div>
-      {pinnedRepoArr?.map((item) => (
-        <div className="repo-item" key={item.id}>
-          <a className="repo-title" href={item.html_url} target="_blank">
-            {item.name}
-          </a>
-          <div className="repo-desc">{item.description}</div>
-          <div className="repo-lan">{item.language}</div>
+    <>
+      {pinnedRepoArr.length > 0 && (
+        <div className="repo-container">
+          <div className="repo-label">My Works</div>
+          {pinnedRepoArr?.map((item) => (
+            <div className="repo-item" key={item.id}>
+              <a className="repo-title" href={item.html_url} target="_blank">
+                {item.name}
+              </a>
+              <div className="repo-desc">{item.description}</div>
+              <div className="repo-lan">{item.language}</div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
--- src/lib/react-floating-profile/ui/components/ProfileModal.tsx
+++ src/lib/react-floating-profile/ui/components/ProfileModal.tsx
@@ -5,8 +5,10 @@ import ReadMe from "./ReadMe";
 import ProfileHeader from "./ProfileHeader";
 import PinnedRepo from "./PinnedRepo";
 import { getUserReadme } from "../../api";
+import { CloseButton } from "../../components/Button";
 
 // 폴더 구조를 지금 컴포넌트 계층구조처럼 만들어보기
+// SagathiyaJaydeep 얘처럼 이름이 없는경우도 있음
 
 export default function ProfileModal({ userObj, pinnedRepoArr }: ModalOption) {
   const { isOpen, setIsOpen } = useProfile();
@@ -23,12 +25,12 @@ export default function ProfileModal({ userObj, pinnedRepoArr }: ModalOption) {
       {isOpen && (
         <div className="modal-container">
           <div className="modal-content">
+            <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
+              <CloseButton />
+            </div>
             <ProfileHeader userObj={userObj} />
             <ReadMe content={readme} />
             <PinnedRepo pinnedRepoArr={pinnedRepoArr!} />
-            <button className="close-btn" onClick={() => setIsOpen(false)}>
-              close
-            </button>
           </div>
         </div>
       )}
