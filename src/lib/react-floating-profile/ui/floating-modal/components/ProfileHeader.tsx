32ae2c1b4cfb1fbb576350299714e2bb6c286060
천짱 <86513078+kangaroo19@users.noreply.github.com>
2025-03-07 15:41:05 +0900

    [REFACTOR] 리드미 이미지 안나오는거 개선

--- src/lib/react-floating-profile/ui/floating-modal/components/ProfileHeader.tsx
+++ src/lib/react-floating-profile/ui/floating-modal/components/ProfileHeader.tsx
@@ -10,10 +10,11 @@ export default function ProfileHeader({ userObj }: ProfileHeaderPropTypes) {
     <div className="profile-header-container">
       <ProfileIcon avatar_url={userObj.avatar_url} size="large" />
       <div style={{ width: "80%", paddingLeft: "10px" }}>
-        <div className="profile-name">{userObj.name}</div>
+        {/*There are cases where the name is missing  */}
+        <div className="profile-name">{userObj.name || userObj.login}</div>
         <div className="profile-bio">{userObj.bio}</div>
-        <div className="profile-login">
-          {userObj.login} {userObj.location && `/ ${userObj.location}`}
+        <div className="profile-login-location">
+          <span className="profile-login">{userObj.login}</span> {userObj.location && `/ ${userObj.location}`}
         </div>
         <div className="profile-bio">{userObj.company}</div>
       </div>
--- src/lib/react-floating-profile/ui/floating-modal/components/ReadMe.tsx
+++ src/lib/react-floating-profile/ui/floating-modal/components/ReadMe.tsx
@@ -2,9 +2,19 @@ import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
 
 interface ReadMePropTypes {
-  content: any;
+  content: string;
 }
 
 export default function ReadMe({ content }: ReadMePropTypes) {
-  return <Markdown className="readme-container" children={content} rehypePlugins={[rehypeRaw]} />;
+  return (
+    <Markdown className="readme-container" children={convertGitHubImageUrls(content)} rehypePlugins={[rehypeRaw]} />
+  );
+}
+
+// 실제 이미지 데이터 가져오도록 하는 코드
+function convertGitHubImageUrls(readmeContent: string): string {
+  return readmeContent.replace(
+    /https:\/\/github\.com\/([^\/]+)\/([^\/]+)\/blob\/([^"]+)/g,
+    "https://raw.githubusercontent.com/$1/$2/refs/heads/$3"
+  );
 }
