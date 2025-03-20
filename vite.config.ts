120a19e6a5f6f46dad5339e15d2f23725494b179
kangaroo19 <1000jjj@naver.com>
2025-03-20 22:57:51 +0900

    [ERROR] context API에서 에러
    - 별칭경로 문제는 vite 설정파일에 alias 설정 추가하여 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@context": path.resolve(__dirname, "src/lib/react-floating-profile/context"),
+      "@api": path.resolve(__dirname, "src/lib/react-floating-profile/api"),
+      "@types": path.resolve(__dirname, "src/lib/react-floating-profile/types"),
+      "@util": path.resolve(__dirname, "src/lib/react-floating-profile/util"),
+      "@components": path.resolve(__dirname, "src/lib/react-floating-profile/components"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom"],
+    },
+  },
 });
