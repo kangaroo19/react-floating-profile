e1cb2ed65e2868813282c0f90523c6e1801f45a2
kangaroo19 <1000jjj@naver.com>
2025-04-10 22:27:52 +0900

    데모 페이지 위한 패키지 설정

---
+++ src/demo/DemoPage.tsx
@@ -0,0 +1,3 @@
+export default function DemoPage() {
+  return <div>데모페이지 입니다.</div>;
+}
--- src/lib/react-floating-profile/ui/ReactFloatingProfile.tsx
+++ src/lib/react-floating-profile/ui/ReactFloatingProfile.tsx
@@ -4,7 +4,8 @@ import "./style.css";
 import FloatingProfileContainer from "./floating-profile-container";
 import { MainOption } from "../types";
 import { AuthProvider, ProfileProvider } from "../context";
-// 에러 발견
+
+// 1. 테마 (다크모드, 라이트모드 등등)
 export default function ReactFloatingProfile({
   userName,
   pinnedRepoArr = [],
