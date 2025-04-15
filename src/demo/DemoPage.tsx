89b2d6c072edf4a3f5e9eb607bf3f272c49e33ff
kangaroo19 <1000jjj@naver.com>
2025-04-15 23:07:27 +0900

    import 에러 수정

--- src/demo/DemoPage.tsx
+++ src/demo/DemoPage.tsx
@@ -1,3 +1,17 @@
+import ReactFloatingProfile from "index";
+
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+
 export default function DemoPage() {
-  return <div>데모페이지 입니다.</div>;
+  return (
+    <div>
+      데모페이지 입니다.{" "}
+      <ReactFloatingProfile
+        accessToken={token}
+        userName="kangaroo19"
+        pinnedRepoArr={["mbti-app", "chatting", "CodingTest", "toDo_List"]}
+        location="top-right"
+      />
+    </div>
+  );
 }
